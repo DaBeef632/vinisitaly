cd32b72ffbb29857ef83db190177de1d149264de
GregConti <gconti63@gmail.com>
2022-03-15 11:41:03 -0400

    set up maria db connection

---
+++ src/main/java/com/vinisitaly/webpage/VinisItalyWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.vinisitaly.webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinisItalyWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinisItalyWebpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinisitaly/webpage/VinisItalyWebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisitaly.webpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinisItalyWebpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
